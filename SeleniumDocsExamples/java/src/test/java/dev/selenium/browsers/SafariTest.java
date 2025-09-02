322302b57bbc707c595f55f02b6de1ce349a41f4
Rick Fencl <rick_fencl@hotmail.com>
2025-09-01 19:06:17 -0700

    Add examples from
    https://www.selenium.dev/documentation/webdriver

---
+++ SeleniumDocsExamples/java/src/test/java/dev/selenium/browsers/SafariTest.java
@@ -0,0 +1,42 @@
+package dev.selenium.browsers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariDriverService;
+import org.openqa.selenium.safari.SafariOptions;
+
+@EnabledOnOs(OS.MAC)
+public class SafariTest {
+    public SafariDriver driver;
+
+    @AfterEach
+    public void quit() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void basicOptions() {
+        SafariOptions options = new SafariOptions();
+        driver = new SafariDriver(options);
+    }
+
+    @Test
+    public void enableLogs() {
+        SafariDriverService service = new SafariDriverService.Builder()
+                .withLogging(true)
+                .build();
+
+        driver = new SafariDriver(service);
+    }
+    
+    public void safariTechnologyPreview() {
+        SafariOptions options = new SafariOptions();
+        options.setUseTechnologyPreview(true);
+        driver = new SafariDriver(options);
+    }
+}
